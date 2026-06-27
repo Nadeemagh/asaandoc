e2179a77227534347ba9c5320e652be4e133e7cb
Nadeemagh <116731793+Nadeemagh@users.noreply.github.com>
2026-06-27 17:22:26 +0500

    Update PatientPortal.js

--- src/pages/PatientPortal.js
+++ src/pages/PatientPortal.js
@@ -35,7 +35,7 @@ export default function PatientPortal() {
         getDoctors(),
         getAppointmentsByPatient(user.uid),
       ]);
-      setDoctors([...docs]);
+      setDoctors(docs.length > 0 ? docs : []);
       setAppointments(appts);
     } catch (e) {
       showToast("Failed to load data. Check Firebase setup.", "error");
